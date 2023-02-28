925fd09060426202f9ac27385b5ccb0eaa9253d8
Yxssssmin <yasmintate02@gmail.com>
2023-03-01 00:51:40 +0100

    Data Jens Segers Yasmin

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,11 +3,17 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Jenssegers\Date\Date;
+
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'login')]
-    public function login(AuthenticationUtils $authenticationUtils)
-    {
+    public function login(AuthenticationUtils $authenticationUtils) {
+
+        Date::setLocale('ca_ES');
+        $hui = Date::now();
+        $data = $hui->format('d') . ', ' . $hui->format('')  ;
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
         
